79fcb753290f20db3eba3bf818f70de8cb6c60f7
ALinkov <aleksanderlinkov@gmail.com>
2018-12-09 15:06:13 +0300

    Surefire plugin

---
+++ calculator/src/test/java/ru/hh/school/calculator/SimpleClass.java
@@ -0,0 +1,17 @@
+package ru.hh.school.calculator;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+/**
+ * Created by ALinkov<br/>
+ * Date: 09.12.2018<br/>
+ */
+public class SimpleClass {
+
+    @Test
+    public void failTest() {
+        fail();
+    }
+}
