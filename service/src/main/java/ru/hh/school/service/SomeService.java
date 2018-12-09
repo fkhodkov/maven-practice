631c66f60ec308072d8e1ced4ef9b0ae75fc0869
ALinkov <aleksanderlinkov@gmail.com>
2018-12-09 14:49:12 +0300

    Initial commit

---
+++ service/src/main/java/ru/hh/school/service/SomeService.java
@@ -0,0 +1,11 @@
+package ru.hh.school.service;
+
+/**
+ * Created by ALinkov<br/>
+ * Date: 26.11.2018<br/>
+ */
+public class SomeService {
+    public double calcPercent() {
+        return 0.;
+    }
+}
